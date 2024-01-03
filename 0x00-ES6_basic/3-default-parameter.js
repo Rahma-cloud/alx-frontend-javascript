0bd65f1258f4270b45ae14afae779b5b88dc4842
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2024-01-03 16:47:21 +0100

    Update task files

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,4 @@
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+    /* eslint-disable no-unused-vars */
     return initialNumber + expansion1989 + expansion2019;
 }
