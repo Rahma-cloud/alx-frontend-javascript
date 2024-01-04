a4f51000f8fadbd5bd46eaf123e01763c01e6462
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2024-01-04 18:52:32 +0100

    Add task files

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,11 @@
+export default function createReportObject(employeesList) {
+  const result = {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employees) {
+      const departments = new Set(Object.keys(employees));
+      return departments.size;
+    },
+  };
+
+  return result;
+}
