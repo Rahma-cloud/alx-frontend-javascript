446548e8b841bd3720095427c31c34d4359a5b92
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2024-01-08 21:17:51 +0100

    Add task files

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const maps = new Map();
+  maps.set('Apples', 10);
+  maps.set('Tomatoes', 10);
+  maps.set('Pasta', 1);
+  maps.set('Rice', 1);
+  maps.set('Banana', 5);
+  return maps;
+}
