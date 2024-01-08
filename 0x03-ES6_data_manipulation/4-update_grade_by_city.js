b8e8fea3cffb0a7705fc27195b3d9a038dde9379
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2024-01-08 19:07:18 +0100

    Add task files

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,23 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+    const filteredStudents = students.filter(student => student.location === city);
+  
+    // Map over the filtered students and update their grades
+    const updatedStudents = filteredStudents.map(student => {
+      const matchingGrade = newGrades.find(grade => grade.studentId === student.id);
+  
+      // If a matching grade is found, update the student's grade; otherwise, set it to 'N/A'
+      if (matchingGrade) {
+        return {
+          ...student,
+          grade: matchingGrade.grade,
+        };
+      } else {
+        return {
+          ...student,
+          grade: 'N/A',
+        };
+      }
+    });
+  
+    return updatedStudents;
+  }
